51b13d60dd42e17f6509b702ed1031eb1102ea07
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2022-06-21 14:39:06 +0200

    update classes

--- src/main/java/at/fhj/msd/Drink.java
+++ src/main/java/at/fhj/msd/Drink.java
@@ -1,25 +1,49 @@
 package at.fhj.msd;
 
-public interface Drink {
+/**
+ * Abstract base class for concrete drink classes
+ */
+public abstract class Drink {
+    /**
+     * name of the drink
+     */
+    protected String name;
 
-        /**
-         * Creates a Drink object with given name
-         */
-        public abstract void Drink(String name);
-        /**
-         * Calculates and returns volume of drink
-         */
-        public abstract double getVolume();
-        /**
-         * Calculates and returns the alcohol percentage
-         */
-        public abstract double getAlcoholPercent();
-        /**
-         * Gives information if drink is alcoholic or not
-         */
-        public abstract boolean isAlcoholic();
+    /**
+     * Creates a Drink object with given name
+     *
+     * @param name name of the drink
+     */
+    public Drink(String name) {
+        this.name = name;
+    }
 
-        public String getName() ;
+    /**
+     * Calculates and returns volume of drink
+     *
+     * @return the volume of drink in liter
+     */
+    public abstract double getVolume();
 
-        public void setName(String name);
+    /**
+     * Calculates and returns the alcohol percentage
+     *
+     * @return alcohol volume percent (e.g. 50)
+     */
+    public abstract double getAlcoholPercent();
+
+    /**
+     * Gives information if drink is alcoholic or not
+     */
+    public abstract boolean isAlcoholic();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
---
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -0,0 +1,98 @@
+package at.fhj.msd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Class that implements a Queue.
+ *
+ */
+public class DrinkQueue{
+    private List<String> elements = new ArrayList<String>();
+    private int maxSize = 5;
+
+
+    public StringQueue(int maxsize) {
+        maxSize = maxSize;
+    }
+
+    /**
+     * <p>Adds an object to a queue, return.</p>
+     *
+     * @param obj added String
+     * @return true if works, otherwise false
+     */
+    @Override
+    public boolean offer(String obj) {
+        if (elements.size() != maxSize)
+            elements.add(obj);
+        else
+            return false;
+
+        return true;
+    }
+
+    /**
+     * <p>Returns and deletes the first element.</p>
+     *
+     * @return first element if exists, otherwise null
+     */
+    @Override
+    public String poll() {
+        String element = peek();
+
+        if (elements.size() != 0) {
+            elements.remove(0);
+        }
+
+        return element;
+    }
+
+    /**
+     * <p>Returns and deletes the first element.</p>
+     *
+     * @throws NoSuchElementException if no element exists
+     * @return first element if exists
+     */
+    @Override
+    public String remove() {
+        String element = poll();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
+
+        return element;
+    }
+
+    /**
+     * <p>Returns the first element.</p>
+     *
+     * @return first element if exists, otherwise null
+     */
+    @Override
+    public String peek() {
+        String element;
+        if (elements.size() > 0)
+            element = elements.get(0);
+        else
+            element = null;
+
+        return element;
+    }
+
+
+    /**
+     * <p>Returns the first element.</p>
+     *
+     * @throws NoSuchElementException if no element exists
+     * @return first element if exists
+     */
+    @Override
+    public String element() {
+        String element = peek();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
+
+        return element;
+    }
+}
