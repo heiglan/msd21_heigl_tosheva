30166f1cc6451fb66421913d4515b8a6c573ec28
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2022-06-21 12:07:04 +0200

    Change method isAlcoholic
    Make small change in method isAlcoholic.

--- src/main/java/at/fhj/msd/SimpleDrink.java
+++ src/main/java/at/fhj/msd/SimpleDrink.java
@@ -7,23 +7,46 @@ public class SimpleDrink implements Drinks{
      */
     protected String name;
 
+    /**
+     * Creates a Drink object with given name
+     *
+     * @param name name of the drink
+     *
+     */
     @Override
     public void Drink(String name) {
 
     }
 
+    /**
+     * Calculates and returns volume of drink
+     *
+     * @return the volume of drink in liter
+     */
     @Override
     public double getVolume() {
         return 0;
     }
 
+    /**
+     * Calculates and returns the alcohol percentage
+     *
+     * @return alcohol volume percent (e.g. 50)
+     */
     @Override
     public double getAlcoholPercent() {
         return 0;
     }
 
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
     @Override
     public boolean isAlcoholic() {
+        if (getAlcoholPercent()==0)
+            return true;
         return false;
     }
 
